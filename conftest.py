5d2578f31da6341dfa4b4405fdac05483157e53c
Marat <kashaevmt@gmail.com>
2019-07-12 21:37:16 +0300

    add fixture for browsers

--- conftest.py
+++ conftest.py
@@ -4,15 +4,28 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-  parser.addoption('--language', action='store', default=None, help="Language option")
+  parser.addoption('--browser_name', action='store', default="chrome",
+                    help="Choose browser: chrome or firefox")
+  parser.addoption('--language', action='store', default="ru",
+                    help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+  browser_name = request.config.getoption("browser_name")
   user_language = request.config.getoption("language")
-  options = Options()
-  options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-  browser = webdriver.Chrome(options=options)
-  
+  if browser_name == "chrome":
+      print("\nstart chrome browser for test..")
+      options = Options()
+      options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+      browser = webdriver.Chrome(options=options)
+  elif browser_name == "firefox":
+      print("\nstart firefox browser for test..")
+      fp = webdriver.FirefoxProfile()
+      fp.set_preference("intl.accept_languages", user_language)
+      browser = webdriver.Firefox(firefox_profile=fp)
+  else:
+      print("Browser {} still is not implemented".format(browser_name))
   yield browser
+  print("\nquit browser..")
   browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,6 +1,10 @@
+
+import time
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_check_button(browser):
   browser.get(link)
   browser.find_element_by_xpath('//form[@class="add-to-basket"]/button')
+  time.sleep(10)
