b342c7998c421a48fe22d18aa0716bdb74771b23
Marat <kashaevmt@gmail.com>
2019-07-12 21:27:46 +0300

    change name of function

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_check_button(browser):
   browser.get(link)
   browser.find_element_by_xpath('//form[@class="add-to-basket"]/button')
