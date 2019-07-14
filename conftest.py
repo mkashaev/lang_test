617ed822f0b12811f47bbd85467ff92df7923e6a
Marat <kashaevmt@gmail.com>
2019-07-14 11:55:05 +0300

    some amendments

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def pytest_addoption(parser):
   parser.addoption('--browser_name', action='store', default="chrome",
                     help="Choose browser: chrome or firefox")
   parser.addoption('--language', action='store', default="ru",
-                    help="Choose browser: chrome or firefox")
+                    help="Choose language")
 
 
 @pytest.fixture(scope="function")
--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_button(browser):
   browser.get(link)
-  browser.find_element_by_xpath('//form[@class="add-to-basket"]/button')
-  time.sleep(10)
+  assert browser.find_element_by_xpath('//form[@class="add-to-basket"]/button')
+  # time.sleep(10)
